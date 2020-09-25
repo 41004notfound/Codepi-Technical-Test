f8b2fd0f99dcc3bcd775971d3f91b026a182d5c2
Lucas Côté - 48022 <48022@etu.he2b.be>
2020-09-25 20:50:53 +0200

    laravel 7.28.3 installation & configuration

---
+++ database/migrations/2020_09_25_183349_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 25);
+            $table->string('description', 225)->default(null);
+            $table->double('price_htva')->default('0');
+            $table->double('price_ttc')->default('0');
+            $table->integer('stock')->default('0');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
