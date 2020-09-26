deabe58b844d59d6f4090b99ac3a970f919d7766
41004notfound <pro@lucascote.fr>
2020-09-26 16:07:42 +0200

    adding footer w/ copyright

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
         <title>{{ env('APP_NAME') ?? 'Codepi Technical Test' }}</title>
 
@@ -23,4 +23,13 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
+    <hr>
+    <footer class="page-footer fixed-bottom">
+        <!-- Copyright -->
+        <div class="footer-copyright text-center bg-white p-3 border-top">
+            © 2020 Copyright - <a target="_blank" href="https://lucascote.fr/"> Lucas Côté</a>.
+        </div>
+        <!-- Copyright -->
+
+    </footer>
 </html>
